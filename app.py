1dfe14b2f46902484897c10cd3f1160316410042
pixielf <pixielf.0@gmail.com>
2018-09-28 14:10:30 -0400

    change app.py to allow run on heroku (not localhost)

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=5000)        # localhost
-    # app.run(host='0.0.0.0', port=port)        # server
+    # app.run(host='127.0.0.1', port=5000)        # localhost
+    app.run(host='0.0.0.0', port=port)        # server
